ec49265bcab926999387e97bf6ba9519b3f98253
wzp <wzp@wzpdeMacBook-Pro.local>
2025-09-23 15:03:39 +0800

    update

---
+++ w1d1/main1.py
@@ -0,0 +1,27 @@
+import hashlib
+import itertools
+import random
+import sys
+import time
+import uuid
+
+YOUR_NAME = "wzp"
+
+start_time = time.time()
+difficulty = 4
+for count in itertools.count(start=0, step=1):
+    # Maxium int
+    nonce = uuid.uuid4() 
+    message = f"{YOUR_NAME}{nonce}".encode('utf-8')
+    hex_dig = hashlib.sha256(message).hexdigest()
+    if hex_dig.startswith("0" * difficulty):
+        print(f"difficulty: {difficulty}")
+        print(f"count: {count}")
+        print(f"Time taken: {time.time() - start_time} seconds")
+        print(f"nonce: {nonce}")
+        print(f"message: {message}")
+        print(f"hex_dig: {hex_dig}")
+        print("--------------------------------")
+        difficulty += 1
+        if difficulty > 5:
+            break
