3caaffb459914336b6e86a363483c8a2f9a52c9d
wzp <wzp@wzpdeMacBook-Pro.local>
2025-09-29 14:06:26 +0800

    update

--- w1d5/tokenBank.sol
+++ w1d5/tokenBank.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "./myerc20.sol";
 
 contract TokenBank {
-    MyERC20 public token;
+    MyERC20 public immutable token;
     mapping(address => uint256) public balances;
 
     constructor(address _token) {
@@ -12,13 +12,18 @@ contract TokenBank {
     }
 
     function deposit(uint256 _amount) public {
-        token.transferFrom(msg.sender, address(this), _amount);
+        require(_amount > 0, "Amount must be greater than 0");
         balances[msg.sender] += _amount;
+        require(
+            token.transferFrom(msg.sender, address(this), _amount),
+            "Transfer failed"
+        );
     }
 
     function withdraw(uint256 _amount) public {
+        require(_amount > 0, "Amount must be greater than 0");
         require(balances[msg.sender] >= _amount, "Insufficient balance");
-        token.transfer(msg.sender, _amount);
+        require(token.transfer(msg.sender, _amount), "Transfer failed");
         balances[msg.sender] -= _amount;
     }
 }
